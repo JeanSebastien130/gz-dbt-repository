450ee76457ba5069835f0aea177330cd3fad1277
JS Morel <jsmorelmorel@gmail.com>
2024-08-07 12:52:27 +0000

    challenge 1

--- models/staging/raw/stg_raw__ship.sql
+++ models/staging/raw/stg_raw__ship.sql
@@ -8,13 +8,7 @@ source as (
 
 renamed as (
 
-    select
-        orders_id,
-        shipping_fee,
-        shipping_fee_1,
-        logcost,
-        ship_cost
-
+    select *
     from source
 )
 
